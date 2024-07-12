ff8528d260cfa1439537dbe5c605b919f9eaf69c
taiga-automate-growth <oshima@automate-growth.com>
2024-07-12 17:48:08 +0900

    GmailApiClientをにJSDocを追加

--- Auto Reply for Google Form Japanese/src/Infrastracture/Api/Clients/GmailApiClient.js
+++ Auto Reply for Google Form Japanese/src/Infrastracture/Api/Clients/GmailApiClient.js
@@ -1,15 +1,26 @@
 class GmailApiClient extends GoogleApiClient{
-    #baseUrl;
-    #apiKey;
+    /** @type {string} */
+	#baseUrl;
+	
+	/** @type {string} */
+	#apiKey;
+	
+	/**
+     * @constructor
+     */
     constructor(){
         super();
-        this.#baseUrl = `https://gmail.googleapis.com/gmail`;
+        this.#baseUrl = 'https://gmail.googleapis.com/gmail';
         this.#apiKey = 'AIzaSyAsZolQRakQXuQOj_Ne7NlXOsrmapJYFWs';
     }
 
+    /** 
+     * @return {object} エイリアスオブジェクト
+     */
     getAiliases(){
-        const parameters = this.createParams('GET');
-        fetch(`${this.#baseUrl}/v1/users/me/settings/sendAs?key=${this.#apiKey}`,parameters)
+        this.parameter.method = 'GET';
+        fetch(`${this.#baseUrl}/v1/users/me/settings/sendAs?key=${this.#apiKey}`,this.parameters)
+        .then(response => response.json())
         .then(ailiases => {return ailiases})
         .catch(error => {throw error});
     }
