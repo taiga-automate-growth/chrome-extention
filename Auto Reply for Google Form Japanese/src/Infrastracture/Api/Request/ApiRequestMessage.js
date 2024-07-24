b8880c90f4d903f29caf13433a46f15980eb72b8
taiga-automate-growth <oshima@automate-growth.com>
2024-07-25 05:08:14 +0900

    change: resourceプロパティとmethodプロパティを削除してrequestDetailプロパティを追加
    クライアント側の分岐を減らすため

---
+++ Auto Reply for Google Form Japanese/src/Infrastracture/Api/Request/ApiRequestMessage.js
@@ -0,0 +1,34 @@
+class ApiRequestMessage{
+    /** @type {string} */
+    #apiName;
+    
+    /** @type {string} */
+    #requestDetail
+    
+    /** 
+     * @constructor
+     * @param {string} apiName
+     * @param {string} requestDetail
+     */
+    constructor(apiName, requestDetail){
+        this.#apiName = apiName;
+        this.#requestDetail = requestDetail
+    }
+
+    /**
+     * @param {Object} params
+     * @return {Object} 
+     */
+    send(params = {}){
+        const message = {
+            apiName: this.#apiName,
+            requestDetail: this.#requestDetail
+        }
+        if(Object.keys(params).length > 0){
+            message.params = params;
+        }
+        chrome.runtime.sendMessage(message)
+        .then(response => {return response;})
+        .catch(error => {return error;})
+    }
+}
