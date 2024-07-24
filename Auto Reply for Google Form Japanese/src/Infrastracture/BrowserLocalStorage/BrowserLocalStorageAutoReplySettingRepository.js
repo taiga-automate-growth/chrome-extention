3f693b0943f1e2180b0209ae0266393a22da8efe
taiga-automate-growth <oshima@automate-growth.com>
2024-07-25 05:26:12 +0900

    rename :リポジトリ

--- Auto Reply for Google Form Japanese/src/Infrastracture/BrowserLocalStorage/BrowserLocalStorageAutoReplySettingRepository.js
+++ Auto Reply for Google Form Japanese/src/Infrastracture/BrowserLocalStorage/BrowserLocalStorageAutoReplySettingRepository.js
@@ -1,4 +1,4 @@
-class AutoReplySettingRepository{
+class BrowserLocalStorageAutoReplySettingRepository{
     #allSettingDatas;
     constructor(){
         chrome.storage.local.get(["auto-reply-google-form-for-japanese"])
@@ -13,13 +13,14 @@ class AutoReplySettingRepository{
     /**
      * 復元
      * 
+     * @param {string} googleFormId GoogleフォームのID
      * @return {AutoReplySetting} 自動返信設定インスタンス 
      */
-    findByGoogleFormId(googleFormId){
+    findByFormId(googleFormId){
         const settingData = allSettingDatas["auto-reply-google-form-for-japanese"][googleFormId];
         if(Object.keys(settingData).length === 0) throw new Error('このフォームに関する情報は保存されていません');
 
-        let scriptId, emailAddressCollectionType, activationStatus, subject, body, fromAddress, fromName, cc, bcc;
+        let scriptId, emailAddressCollectionType, status, subject, body, fromAddress, fromName, cc, bcc, insertContents, aliases;
         if('emailAddressCollectionType' in settingData) emailAddressCollectionType = settingData.emailAddressCollectionSetting;
         if('activationStatus' in settingData) activationStatus = settingData.activationStatus;
         if('scriptid' in settingData) scriptId = settingData.scriptId;
@@ -29,15 +30,16 @@ class AutoReplySettingRepository{
         if('fromName' in settingData) fromName = settingData.fromName;
         if('cc' in settingData) cc = settingData.cc;
         if('bcc' in settingData) bcc = settingData.bcc;
+        if('insertContents' in settingData) insertContents = settingData;
+        if('aliases' in settingData) aliases = settingData.aliases;
 
-        return new AutoReplySetting(googleFormId, scriptId, emailAddressCollectionType,activationStatus, subject, body, fromAddress, fromName, cc, bcc);
+        return new AutoReplySetting(googleFormId, scriptId, emailAddressCollectionType,activationStatus, subject, body, fromAddress, fromName, cc, bcc,insertContents,aliases);
     }
 
     /**
-     * 保存
+     * 永続化
      * 
      * @param {AutoReplySetting} autoReplySetting 自動返信設定インスタンス
-     * @param {}
      */
     save(autoReplySetting){
         const formId = autoReplySetting.getFormId();
