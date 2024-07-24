866560070491071fb9d00402601a7adb4a3f5336
taiga-automate-growth <oshima@automate-growth.com>
2024-07-24 12:26:10 +0900

    add: 自動返信設定モデルを追加

---
+++ Auto Reply for Google Form Japanese/src/Domain/Models/AutoReplySetting.js
@@ -0,0 +1,110 @@
+class AutoReplySetting{
+    /** @type {string} */
+    #formId;
+    
+    /** @type {string} */
+    #scriptId;
+    
+    /** @type {string} */
+    #emailAddressCollectionType;
+    
+    /** @type {boolean} */
+    #status;
+    
+    /** @type {string} */
+    #subject;
+    
+    /** @type {string} */
+    #body;
+    
+    /** @type {string} */
+    #fromAddress;
+    
+    /** @type {string} */
+    #fromName;
+    
+    /** @type {string} */
+    #cc;
+    
+    /** @type {string} */
+    #bcc;
+    
+    /** @type {Array<string>} */
+    #insertContents;
+    
+    /** @type {Array<string>} */
+    #aliases;
+    
+    /** @constructor */
+    constructor(settingDatas){
+        this.#formId = settingDatas.formId ?? "";
+        this.#scriptId = settingDatas.scriptId ?? "";
+        this.#emailAddressCollectionType = settignDatas.emailAddressCollectionSetting ?? "";
+        this.#status = settingDatas.status ?? "";
+        this.#subject = settingDatas.subject ?? "";
+        this.#body = settingDatas.body ?? "";
+        this.#fromAddress = settingDatas.fromAddress ?? "";
+        this.#fromName = settingDatas.fromName ?? "";
+        this.#cc = settingDatas.cc ?? "";
+        this.#bcc = settingDatas.bcc ?? "";
+        this.#insertContents = settingDatas.insertContents ?? "";
+        this.#aliases = settingDatas.aliases ?? "";
+    }
+    
+    /**
+     * @return {Object} settingDatas
+     */
+    getAsObject(){
+     	return {
+    		formId:this.#formId,
+    		scriptId:this.#scriptId,
+ 		    emailAddressCollectionType: this.#emailAddressCollectionType,
+    		activationStatus: this.#activationStatus,
+		    subject: this.#subject,
+		    body: this.#body,
+		    fromAddress: this.#fromAddress,
+		    fromName: this.#fromName,
+		    cc: this.#cc,
+    		bcc: this.#bcc,
+    		insertContents:this.#insertContents,
+    		aliases:this.#aliases
+     	}
+     }
+
+	/**
+	 * 
+	 */
+	activate(){
+		this.#status = true;
+	}
+	
+	/**
+	 * @param {Array<string>} insertContents 
+	 */
+	setInsertContents(insertContents){
+		this.#insertContents = insertContents;
+	}
+	
+	/**
+	 * @param {Array<string>} aliases
+	 */
+	setAliases(aliases){
+		this.#aliases = aliases;
+	}
+	
+	/**
+	 * @return {Boolean}
+	 */
+	isCollectEmail(){
+		if(this.#emailCollectionType === "収集しない") return false;
+		return true;
+	}
+	
+	/**
+	 * @return {Boolean}
+	 */
+	isRequiredEmpty(){
+		if(this.#subject === "" || this.#body === "" || this.#fromAddress === "") return true;
+		return false;
+	}
+}
