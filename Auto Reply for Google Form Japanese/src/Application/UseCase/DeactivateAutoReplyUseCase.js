e53952b8507d793f49ddf3cbbd1129a7818239dd
taiga-automate-growth <oshima@automate-growth.com>
2024-07-26 16:18:34 +0900

    add:自動返信を無効にするユースケースを追加

---
+++ Auto Reply for Google Form Japanese/src/Application/UseCase/DeactivateAutoReplyUseCase.js
@@ -0,0 +1,38 @@
+class DeactivateAutoReplyUseCase{
+
+	handle(formId){
+		const repository = new BrowserLocalStorageAutoReplySettingRepository();
+		try{
+			const autoReplySetting = repository.findByFormId(formId);
+			autoReplySetting.deactivate();
+			repository.save(autoReplySetting);
+			
+			if(!autoReplySetting.hasScript()){
+				throw new NotFoundScriptException();
+			}
+			const manifestFile = {
+	            name: 'appsscript',
+	            type: 'JSON',
+	            source:JSON.stringify({
+	                "timeZone": "Asia/Tokyo",
+	                "dependencies": {},
+	                "exceptionLogging": "STACKDRIVER",
+	                "runtimeVersion": "V8",
+	                "oauthScopes":[
+	                    "https://mail.google.com/",
+	                    "https://www.googleapis.com/auth/script.scriptapp",
+	                    "https://www.googleapis.com/auth/forms"
+	                ]
+	            })
+	        }
+			
+			new ApiRequestMessage('Apps Script', 'updateScript')
+			.send({scriptId: autoReplySetting.getScriptId(), files[manifestFile]})
+			.then(res => {})
+			.catch(e => throw e);
+			
+		}catch(e){
+
+		}
+	}
+}
