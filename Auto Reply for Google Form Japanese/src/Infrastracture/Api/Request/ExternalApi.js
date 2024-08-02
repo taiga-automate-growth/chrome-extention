ccadc13cd75c358b37c56e5962d81bdc53f53c17
taiga-automate-growth <oshima@automate-growth.com>
2024-08-02 14:31:29 +0900

    add: ApiRequesterから名称変更 外部APIを表すクラス

---
+++ Auto Reply for Google Form Japanese/src/Infrastracture/Api/Request/ExternalApi.js
@@ -0,0 +1,69 @@
+import { GoogleFormApiClient } from "../Clients/GoogleFormApiClient.js";
+import { GmailApiClient } from "../Clients/GmailApiClient.js";
+import { GoogleAppsScriptApiClient } from "../Clients/GoogleAppsScriptApiClient.js";
+
+export class ExternalApi{
+
+    /** @type {Object} */
+    #message;
+    
+    /** @type {Object} */
+    #sender;
+    
+    /** @callback Response */
+    /** @type {Response} */
+    #response;
+
+    /**
+     * @constructor
+     */
+    constructor(message, sender, sendResponse){
+        this.#message = message;
+        this.#sender = sender;
+        this.#response = sendResponse;
+    }
+
+	/**
+	 * @param {string} apiName
+	 * @return {GoogleApiClient}
+	 */
+    #generateApiClient(apiName){
+        switch(apiName){
+            case 'Google Form':
+                return new GoogleFormApiClient();
+                break;
+            case 'Gmail':
+                return new GmailApiClient();
+                break;
+            case 'Apps Script':
+                return new GoogleAppsScriptApiClient();
+                break;
+            default:
+                throw new ApiClientNotFoundException();
+                break;
+        }
+    }
+    
+    /** */
+    request(){
+        
+        const apiClient = this.#generateApiClient(this.#message.apiName);
+            
+        if(this.#message.action === 'getForm'){
+            
+	        this.#response(apiClient.getForm());
+            
+        }else if(this.#message.action === 'getAliases'){
+        
+	        this.#response(apiClient.getAlieses());
+            
+        }else if(this.#message.action === 'createScript'){
+        
+	        this.#response(apiClient.createScript());
+	        
+        }else if(this.#message.action === 'updateScript'){
+        
+	        this.#response(apiClient.updateScript());
+        }
+    }
+}
