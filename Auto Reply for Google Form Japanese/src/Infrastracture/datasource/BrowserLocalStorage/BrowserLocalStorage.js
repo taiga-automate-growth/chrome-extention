bf699851a70ddef8efd26fc2ae807561d9838c19
taiga-automate-growth <oshima@automate-growth.com>
2024-08-05 04:35:27 +0900

    update:ブラウザローカルストレージのデータ入出力時にPromiseを返却するように変更

--- Auto Reply for Google Form Japanese/src/Infrastracture/datasource/BrowserLocalStorage/BrowserLocalStorage.js
+++ Auto Reply for Google Form Japanese/src/Infrastracture/datasource/BrowserLocalStorage/BrowserLocalStorage.js
@@ -1,15 +1,30 @@
 export class BrowserLocalStorage{
+    /**
+     * 
+     * @param {string} key ローカルストレージに保存されるキー
+     * @return {Promise} 
+     */
     get(key){
-        chrome.storage.local.get(key)
-        .then(data => {return data})
-        .catch(e => {throw e});
+        console.log('ブラウザローカルストレージから値を取り出します');
+        return new Promise((resolve,reject)=>{
+            chrome.storage.local.get(key)
+            .then(data => {
+                if(Object.keys(data).length === 0) {
+                    reject(NotFoundDataException(`ローカルストレージに${key}というデータは存在しませんでした`));
+                    return;
+                }
+                console.log('ブラウザローカルストレージから取り出したデータです');
+                console.log(data);
+                resolve(data)})
+            .catch(e => {reject(e)});
+        });
     }
 
     save(data){
-        chrome.storage.local.set(data)
-        .then(() => {})
-        .catch(() => {
-            throw new Error('データの保存に失敗しました');
-        });
+        return new Promise((resolve,reject) => {
+            chrome.storage.local.set(data)
+            .then(() => {resolve('データ保存成功')})
+            .catch(() => {reject(new Error('データの保存に失敗しました'));});
+        })
     }
 }
