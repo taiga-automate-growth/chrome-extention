231a115b0551be11589873f194762b2da9f39a52
taiga-automate-growth <oshima@automate-growth.com>
2024-08-05 11:14:02 +0900

    update:apiNameが小文字でも対応できるように条件を追加

--- Auto Reply for Google Form Japanese/src/Infrastracture/Api/Request/ExternalApi.js
+++ Auto Reply for Google Form Japanese/src/Infrastracture/Api/Request/ExternalApi.js
@@ -2,6 +2,7 @@ import { GoogleFormApiClient } from "../Clients/GoogleFormApiClient.js";
 import { GmailApiClient } from "../Clients/GmailApiClient.js";
 import { GoogleAppsScriptApiClient } from "../Clients/GoogleAppsScriptApiClient.js";
 import { GoogleApiClient } from "../Clients/GoogleApiClient.js";
+import { ApiClientNotFoundException } from "../../../Exceptions/ApiClientNotFoundException.js";
 
 export class ExternalApi{
 
@@ -21,6 +22,7 @@ export class ExternalApi{
         this.#apiName = message.resource;
         this.#action = message.action;
         this.#params = message.params ?? {};
+        console.log(message);
     }
 
 	/**
@@ -30,12 +32,16 @@ export class ExternalApi{
     #generateApiClient(apiName){
         switch(apiName){
             case 'Google Form':
+            case 'form':
+            case 'Form':
                 return new GoogleFormApiClient();
                 break;
             case 'Gmail':
+            case 'gmail':
                 return new GmailApiClient();
                 break;
             case 'Apps Script':
+            case 'Google Apps Script':
                 return new GoogleAppsScriptApiClient();
                 break;
             default:
@@ -48,27 +54,41 @@ export class ExternalApi{
      * @return {Promise}
      */
     request(){
-        
+        console.log('外部APIにリクエストを送ります')
         const apiClient = this.#generateApiClient(this.#apiName);
-
+        console.log(apiClient);
         if(this.#action === 'getForm'){
-            
+            console.log('フォームを取得します');
             return new Promise((resolve, reject) => {
-                apiClient.getForm()
-                .then(form => resolve(form))
-                .catch(error => reject(error));
+                apiClient.getForm(this.#params.formId)
+                .then(form => {
+                    console.log('フォームを取得しました');
+                    console.log(form);
+                    resolve(form)
+                })
+                .catch(error => {
+                    console.log('フォームの取得に失敗しました');
+                    console.log(error);
+                    reject(error)
+                });
             });
             
         }else if(this.#action === 'getAliases'){
-
+            console.log('エイリアスを取得します');
             return new Promise((resolve,reject) =>{
-                apiClient.getAlieses()
-                .then(aliases => resolve(aliases))
-                .catch(error => reject(error));
+                apiClient.getAliases()
+                .then(aliases => {
+                    console.log('エイリアスを取得しました');
+                    console.log(aliases);
+                    resolve(aliases)})
+                .catch(error => {
+                    console.log('エイリアスの取得に失敗しました');
+                    reject(error)});
             });
 
         }else if(this.#action === 'createScript'){
-            
+
+            console.log('スクリプトを作ります');
             return new Promise((resolve,reject) =>{
                 apiClient.createScript(this.#params.title, this.#params.scriptId)
                 .then(project => resolve(project))
@@ -77,6 +97,7 @@ export class ExternalApi{
 	        
         }else if(this.#action === 'updateScript'){
         
+            console.log('スクリプトを更新します');
             return new Promise((resolve,reject) =>{
                 apiClient.updateScript(this.#params.scriptId, this.#params.files)
                 .then(project => resolve(project))
