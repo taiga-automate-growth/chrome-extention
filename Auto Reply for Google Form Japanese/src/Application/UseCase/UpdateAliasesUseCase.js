43b5b6d707e9f7845b0d322598056bee9acffc0b
taiga-automate-growth <oshima@automate-growth.com>
2024-08-08 09:29:18 +0900

    update:非同期に変更して、Promiseを返却

--- Auto Reply for Google Form Japanese/src/Application/UseCase/UpdateAliasesUseCase.js
+++ Auto Reply for Google Form Japanese/src/Application/UseCase/UpdateAliasesUseCase.js
@@ -1,26 +1,30 @@
+import { BackgroundMessage } from '../../Infrastracture/background/BackgroundMessage.js';
 import {BrowserLocalStorageAutoReplySettingRepository} from '../../Infrastracture/datasource/BrowserLocalStorage/BrowserLocalStorageAutoReplySettingRepository.js';
-import {ApiRequestMessage} from '../../Infrastracture/Api/Request/ApiRequestMessage.js';
+
 
 export class UpdateAliasesUseCase{
-	handle(formId){
-		new ApiRequestMessage('Gmail', 'getAliases').send()
-		.then(gmail => {
-			
-			const repository = new BrowserLocalStorageAutoReplySettingRepository();
-			
-			try{
-			
-				const autoReplySetting = repository.findByFormId(formId);
-				const aliases = gmail.sendAs;
-				autoReplySetting.setAliases(aliases);
-				repository.save(autoReplySetting);
-				return aliases;
-				
-			}catch(e){
-				throw e;
-			}
+	/**
+	 * 
+	 * @param {string} formId - GoogleフォームのID
+	 * @return 
+	 */
+	async handle(formId){
 		
-		})
-		.catch(e => {throw e});
+		const repository = new BrowserLocalStorageAutoReplySettingRepository();
+		
+		try{
+			
+			const autoReplySetting = await repository.findByFormId(formId);
+			const gmail = await new BackgroundMessage('ApiRequest', 'Gmail', 'getAliases').send();
+			const aliases = gmail.sendAs.map(alias => {return alias.sendAsEmail});
+			autoReplySetting.setAliases(aliases);
+			repository.save(autoReplySetting);
+			return {aliases: aliases};
+			
+		}catch(e){
+			throw e;
+		}
+	
+
 	}
 }
