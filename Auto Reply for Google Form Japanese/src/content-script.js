2c30878fc058d3c51b7c11c18fbc05f1a8e1341e
taiga-automate-growth <oshima@automate-growth.com>
2024-08-16 06:45:55 +0900

    rename:contents-script→App,loader→content-scriptに

--- Auto Reply for Google Form Japanese/src/content-script.js
+++ Auto Reply for Google Form Japanese/src/content-script.js
@@ -1,5 +1,5 @@
 (() => {
-    const src = chrome.runtime.getURL('src/contents-script.js');
+    const src = chrome.runtime.getURL('src/App.js');
     console.log(src);
     const script = document.createElement('script');
     script.setAttribute('src', src);
