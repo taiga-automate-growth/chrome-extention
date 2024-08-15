acfbe23048cc0aa7b924db923921d5273db10964
taiga-automate-growth <oshima@automate-growth.com>
2024-08-16 06:15:56 +0900

    update:ユースケースをコンストラクタインジェクションに変更 DIにより依存を注入する為

--- Auto Reply for Google Form Japanese/src/Presentation/Controllers/AutoReplySettingController.js
+++ Auto Reply for Google Form Japanese/src/Presentation/Controllers/AutoReplySettingController.js
@@ -13,19 +13,57 @@ export class AutoReplySettingController{
 	
 	/** @type {AutoReplySettingView} */
 	#view;
+
+	/** @type {ActivateAutoReplyUseCase} */
+	#activateUseCase;
+
+	/** @type {CreateScriptUseCase} */
+	#createScriptUseCase;
+
+	/** @type {DeactivateAutoReplyUseCase} */
+	#deactivateUseCase;
+
+	/** @type {GetAutoReplySettingUseCase} */
+	#getUseCase;
+
+	/** @type {UpdateAliasesUseCase} */
+	#updateAliasesUseCase;
+
+	/** @type {UpdateInsertContentsUseCase} */
+	#updateInsertContentsUseCase;
 	
 	/**
 	 * @constructor
-	 * @param {AutoReplySettingView} view
+	 * @param {AutoReplySettingView} view - ビューインスタンス
+	 * @param {ActivateAutoReplyUseCase} activateUseCase
+	 * @param {CreateScriptUseCase} createScriptUseCase
+	 * @param {DeactivateAutoReplyUseCase} deactivateUseCase
+	 * @param {GetAutoReplySettingUseCase} getUseCase
+	 * @param {UpdateAliasesUseCase} updateAliasesUseCase
+	 * @param {UpdateInsertContentsUseCase} updateInsertContentsUseCase
 	 */
-	constructor(view){
+	constructor(
+		view,
+		activateUseCase,
+		createScriptUseCase,
+		deactivateUseCase,
+		getUseCase,
+		updateAliasesUseCase,
+		updateInsertContentsUseCase
+	){
 		this.#view = view;
+		this.#activateUseCase = activateUseCase;
+		this.#createScriptUseCase = createScriptUseCase;
+		this.#deactivateUseCase = deactivateUseCase;
+		this.#getUseCase = getUseCase;
+		this.#updateAliasesUseCase = updateAliasesUseCase;
+		this.#updateInsertContentsUseCase = updateInsertContentsUseCase;
 	}
 	
 	async index(){
 		this.#view.loadStart('自動返信設定項目を追加しています...')
 		const formId = this.#view.getFormId();
-		const settingData = await new GetAutoReplySettingUseCase().handle(formId);
+		const settingData = await this.#getUseCase.handle(formId);
 		try{
 		
 			this.#view.update(settingData);
@@ -53,7 +91,7 @@ export class AutoReplySettingController{
 	
 	async activate(formId){
 		this.#view.loadStart('自動返信を有効にしています...')
-		const activationData = await new ActivateAutoReplyUseCase().handle(formId);
+		const activationData = await this.#activateUseCase.handle(formId);
 		this.#view.update(activationData);
 		this.#view.activate();
 		this.#view.loadEnd();
@@ -62,9 +100,10 @@ export class AutoReplySettingController{
 	async createScript(formId,inputData){
 		this.#view.loadStart('プログラムを生成しています...')
 		try {
-			const scriptId = await new CreateScriptUseCase().handle(formId,inputData);
+			const scriptId = await this.#createScriptUseCase.handle(formId,inputData);
 			this.#view.createScriptDone(scriptId);
 		} catch (error) {
+			console.log(error);
 			this.#errorHandle(error);
 		}
 		this.#view.loadEnd();
@@ -72,21 +111,21 @@ export class AutoReplySettingController{
 	
 	async deactivate(formId){
 		this.#view.loadStart('自動返信を無効にしています...')
-		await new DeactivateAutoReplyUseCase().handle(formId);
+		await this.#deactivateUseCase.handle(formId);
 		this.#view.deactivate();
 		this.#view.loadEnd();
 	}
 	
 	async updateInsertContents(formId){
 		this.#view.loadStart('差し込みコンテンツを取得しています...')
-		const insertContents = await new UpdateInsertContentsUseCase().handle(formId);
+		const insertContents = await this.#updateInsertContentsUseCase.handle(formId);
 		this.#view.update(insertContents);
 		this.#view.loadEnd();
 	}
 	
 	async updateAliases(formId){
 		this.#view.loadStart('送信元アドレスを取得しています...');
-		const aliases = await new UpdateAliasesUseCase().handle(formId);
+		const aliases = await this.#updateAliasesUseCase.handle(formId);
 		this.#view.update(aliases);
 		this.#view.loadEnd();
 	}
